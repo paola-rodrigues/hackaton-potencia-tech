a12dc78d9ceefe94fdf180c6baf01af3f4ebb466
Renan Araújo <renan.araujoonline@gmail.com>
2023-05-27 18:45:20 -0300

    created bottom header

--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,23 @@ export default function Home() {
               </div>
             </div>
           </div>
+          <div id='bottom-header' className='flex h-[101px] bg-[#AF2B36] justify-between'>
+            <div className='ml-5'>
+              <Image src="/logo.png" alt="Logo" width={100} height={100}/>
+            </div>
+            <div className='flex items-center gap-[2rem] mr-5'>
+              <p>Sobre</p>
+              <p>Cursos</p>
+              <p>Blog</p>
+              <p>Mentoria</p>
+              <p>Projetos</p>
+              <p>Livros</p>
+              <p>Contatos</p>
+              <button className='bg-[white] p-3 w-[8rem] rounded-[10px]'>
+                LOGIN
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </main>
